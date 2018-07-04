15716d0e6c7f65be1d7ed1896cdbb5da89231aee
amay077 <okuokuoku@gmail.com>
2018-07-05 00:58:43 +0900

    update: gilpfile

--- messages/gulpfile.js
+++ messages/gulpfile.js
@@ -1,22 +1,14 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var babel = require('gulp-babel');
-var ts = require('gulp-typescript');
+var gulp = require("gulp");
+var typescript = require('gulp-typescript');
 
-tsProject = ts.createProject('tsconfig.json', function() {
-  typescript: require('typescript')
+gulp.task("default", () => {
+  var pj = typescript.createProject("./tsconfig.json");
+
+  gulp.src([
+      "./**/*.ts",
+      "!./node_modules/**"
+    ])
+    .pipe(pj())
+    .js
+    .pipe(gulp.dest("./"));
 });
-
-gulp.task('ts', function() {
-  tsProject.src('src/*.ts')
-    .pipe(ts(tsProject))
-    .pipe(babel())
-    .pipe(uglify())
-    .pipe(gulp.dest('dist'))
-});
-
-gulp.task('watch', function() {
-  gulp.watch('src/*.ts', ['ts'])
-});
-
-gulp.task('default', ['ts', 'watch']);
